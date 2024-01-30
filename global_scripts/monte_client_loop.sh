c985a15eb917d816f58da36e65a2ba9009eb6a66
kevin00 <kevin00@mit.edu>
2024-01-30 14:21:49 -0500

    bug

--- global_scripts/monte_client_loop.sh
+++ global_scripts/monte_client_loop.sh
@@ -3,9 +3,8 @@
 
 ME="monte_client_loop.sh"
 PERPETUAL=""
-RE_UPDATE="yes"
 IGNORE_WARNING="no"
-HOSTLESS="no"
+HOSTLESS=""
 SLEEP_TIME=60
 
 # shellcheck disable=SC1090
@@ -34,7 +33,7 @@ for ARGI; do
         echo "            running in a cluster.     "
         exit 0
     elif [[ "${ARGI}" = "--hostless" || "${ARGI}" = "-nh" ]]; then
-        HOSTLESS="yes"
+        HOSTLESS=""--hostless""
     elif [[ "${ARGI}" = "-p" ]]; then
         PERPETUAL="yes"
     elif [[ "${ARGI}" = "-y" ]]; then
@@ -74,9 +73,6 @@ if [ -d "job_dirs" ] && [ "$MYNAME" != "$MONTE_MOOS_HOST" ]; then
     fi
 fi
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - -
-#  Part 1c: Check if this is the host
-#- - - - - - - - - - - - - - - - - - - - - - - - - -
 if [ "$MYNAME" = "$MONTE_MOOS_HOST" ]; then
     vexit "This script should only be run on a client" 1
 fi
@@ -90,17 +86,9 @@ if [ -f "${CARLO_DIR_LOCATION}/force_quit" ]; then
 fi
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - -
-# Part 1e: Set the flags for run_next.sh
+# Part 1e: Ensure the temporary job dirs are in place
 #- - - - - - - - - - - - - - - - - - - - - - - - - -
-FLAGS=""
-if [ "$RE_UPDATE" = "yes" ]; then
-    RE_UPDATE=""
-    day_of_last_update=$(date +%u)
-    FLAGS+=" --update"
-fi
-if [ "$HOSTLESS" = "yes" ]; then
-    FLAGS+=" --hostless"
-
+if [ "$HOSTLESS" = "--hostless" ]; then
     # If the runtime job_dirs directory doesn't exist, copy the local_job_dirs
     if [ ! -d "${CARLO_DIR_LOCATION}/job_dirs" ]; then
         if [ -d "${CARLO_DIR_LOCATION}/local_job_dirs" ]; then
@@ -114,21 +102,26 @@ fi
 #- - - - - - - - - - - - - - - - - - - - - - - - - -
 monte_clean.sh --cache
 
+RE_UPDATE="--update"
 #-------------------------------------------------------
 #  Part 2: Run, and (sometimes) update
 #-------------------------------------------------------
 while true; do
 
+
     #- - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Clean from the last run
+    # Clean and reupdate
+    if [ "$RE_UPDATE" = "--update" ]; then
+        RE_UPDATE=""
+        day_of_last_update=$(date +%u)
+    fi
     monte_clean.sh
 
     #- - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Run the next job, updating if necessary
-    /"${MONTE_MOOS_BASE_DIR}"/client_scripts/run_next.sh $FLAGS
+    # Run the next job and check exit codes
+    /"${MONTE_MOOS_BASE_DIR}"/client_scripts/run_next.sh $HOSTLESS $RE_UPDATE
     EXIT=$?
 
-    #- - - - - - - - - - - - - - - - - - - - - - - - - -
     # Exit code 1: Queue is empty
     if [ $EXIT -eq 1 ]; then
         if [ -z "$PERPETUAL" ]; then
@@ -153,14 +146,14 @@ while true; do
     #- - - - - - - - - - - - - - - - - - - - - - - - - -
     # Exit code !=0: run next failed for unknown reason
     elif [ $EXIT -ne 0 ]; then
-        vexit "/${MONTE_MOOS_BASE_DIR}/client_scripts/run_next.sh $FLAGS failed with code $EXIT" 2
+        vexit "/${MONTE_MOOS_BASE_DIR}/client_scripts/run_next.sh $HOSTLESS $RE_UPDATE failed with code $EXIT" 2
     fi
     echo ""
 
     #- - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Determine if the client should update
-    if [ $day_of_last_update -ne $(date +%u) ]; then
-        RE_UPDATE="yes"
+    # Determine if the client should update (daily)
+    if [ "$day_of_last_update" -ne $(date +%u) ]; then
+        RE_UPDATE="--update"
     fi
 
     #- - - - - - - - - - - - - - - - - - - - - - - - - -
